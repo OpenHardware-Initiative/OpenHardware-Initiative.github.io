5ac8855edd88f56576d302c9f67c7bd30d74bf90
Deniz Öztürk <750282004C4DEC44@mytum.de>
2025-10-26 11:11:14 +0100

    Update organization name in About page

--- Source code/src/pages/About.tsx
+++ Source code/src/pages/About.tsx
@@ -210,7 +210,7 @@ const About = () => {
                   <h3 className="text-xl font-bold text-primary-blue">2025</h3>
                   <p className="font-medium">Foundation of the OpenHardware Initiative</p>
                   <p className="text-gray-600 mt-2">
-                    OpenHardware is founded as a student initiative under the OpenResearch e.V. by Ipek Akdeniz, 
+                    OpenHardware is founded as a student initiative under the TukiResearch e.V. by Ipek Akdeniz, 
                     Deniz Onat Erdogan, Aleksa Stojkovic and Agustin N. Coppari Hollmann.
                   </p>
                 </div>
