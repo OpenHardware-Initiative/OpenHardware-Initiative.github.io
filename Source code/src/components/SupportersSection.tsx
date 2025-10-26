5ba901ef9518b9de4c6ee1306d08e44e8c5d9e97
Deniz Öztürk <750282004C4DEC44@mytum.de>
2025-10-26 14:53:20 +0100

    Change background color of SupportersSection

--- Source code/src/components/SupportersSection.tsx
+++ Source code/src/components/SupportersSection.tsx
@@ -32,7 +32,7 @@ const partners: Partner[] = [
 
 const SupportersSection = () => {
   return (
-    <section className="py-16 bg-offwhite">
+    <section className="py-16 bg-primary-blue/5">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-12 text-primary-blue">
           <span className="relative pb-2">
