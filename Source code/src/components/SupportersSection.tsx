a8b48fdb9ac746f2a17b335dd0040158de9ec2c6
Deniz Öztürk <750282004C4DEC44@mytum.de>
2025-10-26 13:27:32 +0100

    Update logo source path for TukiResearch

--- Source code/src/components/SupportersSection.tsx
+++ Source code/src/components/SupportersSection.tsx
@@ -20,7 +20,7 @@ const partners: Partner[] = [
     name: "TukiResearch e.V.",
     role: "Non-Profit Organization",
     organization: "",
-    logoSrc: "/IMG_3429.jpg"
+    logoSrc: "/lovable-uploads/IMG_3429.jpg"
   }, 
   {
     name: "neuroTUM",
